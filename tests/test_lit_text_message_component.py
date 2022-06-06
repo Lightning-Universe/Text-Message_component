8be10978ca918758e6d490c2a0c26b7578aefc94
William Falcon <waf2107@columbia.edu>
2022-06-05 23:50:29 -0400

    add tests

--- tests/test_lit_text_message_component.py
+++ tests/test_lit_text_message_component.py
@@ -8,6 +8,7 @@ def test_send_message():
     twilio_sid = os.environ['TEST_TWILIO_SID']
     twilio_auth = os.environ['TEST_TWILIO_AUTH_TOKEN']
     twilio_phone = os.environ['TEST_TWILIO_PHONE_NUMBER']
+    receiver_phone = os.environ['TEST_TWILIO_RECEIVER_PHONE_NUMBER']
 
     lit_text_message = TextMessage(
         twilio_phone=twilio_phone, 
@@ -16,7 +17,7 @@ def test_send_message():
     )
 
     with io.StringIO() as buf, redirect_stdout(buf):
-        lit_text_message.send_text('hi ⚡ from lightning ⚡', to=[twilio_phone])
+        lit_text_message.send_text('hi ⚡ from lightning ⚡', to=[receiver_phone])
         output = buf.getvalue()
         print(output)
         assert 'message id:' in output
